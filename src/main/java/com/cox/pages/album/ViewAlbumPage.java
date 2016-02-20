75331a1088dc5dd86e451eb0071b0bc40daf304e
Alexander Silaev <gavuss@gmail.com>
2016-02-20 19:55:34 +0300

    add PopUp

---
+++ src/main/java/com/cox/pages/album/ViewAlbumPage.java
@@ -0,0 +1,7 @@
+package com.cox.pages.album;
+
+/**
+ * Created by silaev on 2/20/16.
+ */
+public class ViewAlbumPage {
+}
---
+++ src/main/java/com/cox/pages/cox/elements/CoxPopUp.java
@@ -0,0 +1,7 @@
+package com.cox.pages.cox.elements;
+
+/**
+ * Created by silaev on 2/20/16.
+ */
+public class CoxPopUp {
+}
